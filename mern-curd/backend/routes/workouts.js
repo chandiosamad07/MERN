ab5227011f000b9e9179538a8d97b676a21e253e
chandiosamad07 <absamadchandio@gmail.com>
2024-03-27 13:38:54 +0500

    get a single workout

--- mern-curd/backend/routes/workouts.js
+++ mern-curd/backend/routes/workouts.js
@@ -7,6 +7,10 @@ router.get('/' ,(req , res)=>{
    res.json({mesg:"Get all workouts"})
 })
 
-
+//Get a single workout
+router.get('/:id' ,(req , res)=>{
+    res.json({mesg:"Get a Single workout"})
+ })
+ 
 
 module.exports = router
