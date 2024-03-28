9c077279e2ca457f68252c79e8a44caa88ed577c
chandiosamad07 <absamadchandio@gmail.com>
2024-03-28 16:34:51 +0500

    Setup Mongodb data base

--- mern-curd/backend/server.js
+++ mern-curd/backend/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require("express");
+const mongoose =require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 //express App
@@ -12,8 +13,17 @@ app.use(express.json())
 //routes
 app.use('/api/workouts',workoutRoutes)
 
+//connect to DB
 
-// listen for requests
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+
+    // listen for requests
 app.listen(process.env.PORT,()=>{
     console.log("Listening on port" , process.env.PORT)
 })
+})
+.catch((error)=>{
+console.log(error)
+})
+
