e43c0bf70495969e443826d6540163635b51ea70
chandiosamad07 <absamadchandio@gmail.com>
2024-04-01 19:46:53 +0500

    Backend completed for CURD APP

--- mern-curd/backend/server.js
+++ mern-curd/backend/server.js
@@ -13,3 +13,16 @@ app.use(express.json())
 app.use('/api/workouts',workoutRoutes)
 
 //connect to DB
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+
+    // listen for requests
+app.listen(process.env.PORT,()=>{
+    console.log("Listening on port" , process.env.PORT)
+})
+})
+.catch((error)=>{
+console.log(error)
+})
+
