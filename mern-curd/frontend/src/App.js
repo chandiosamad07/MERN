17a6044bbf341ba22417b2bb5fd0a2f7ca76ec2b
chandiosamad07 <absamadchandio@gmail.com>
2024-04-22 10:11:14 +0500

    NavBar page

--- mern-curd/frontend/src/App.js
+++ mern-curd/frontend/src/App.js
@@ -1,10 +1,12 @@
 import {BrowserRouter , Routes,Route} from 'react-router-dom'
 import Home from './Pages/Home';
+import Navbar from './Componets/Navbar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar />
       <div className='pages'>
         <Routes>
           <Route 
